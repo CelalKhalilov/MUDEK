5b936f4f83cd0b7b837b661c7e1f4b8f5e4cc219
Enes Sahin <enes.sahin03es@gmail.com>
2024-05-08 19:46:55 +0300

    Gitignore oluşturuldu.

--- ProjectOfMudekk/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ ProjectOfMudekk/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68e551bccaa9f1e96a3edb54e6382217ff0d292b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d11931a36ee5325db4014b1b1150b46ac362e4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
